0f8f4204543fdd9b1779261d4e7739182ee702ce
DevilEars <49192883+DevilEars@users.noreply.github.com>
2020-02-11 17:05:30 +0200

    Snake divided into classes
    Still need to make wall collisions work.

---
+++ Snake/Board.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+This is the board for the snake
+
+This only checks for collisions
+
+@author: devilears
+"""
+
+class Board:
+        
+    def isCollision(self,x1,y1,x2,y2,bsize):
+        if x1 >= x2 and x1 <= x2 + bsize:
+            if y1 >= y2 and y1 <= y2 + bsize:
+                return True
+        return False
---
+++ Snake/SnakeApp.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 16:56:40 2020
+
+@author: devilears
+"""
+
+from pygame.locals import QUIT, K_RIGHT, K_LEFT, K_UP, K_DOWN, K_ESCAPE
+from random import randint
+import pygame
+import time
+
+# TÖDO This works but it seems clunky
+from Apple import Apple
+from Player import Player
+from Board import Board
+
+ 
+class SnakeApp:
+ 
+    windowWidth = 800
+    windowHeight = 600
+    player = 0
+    apple = 0
+ 
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self._apple_surf = None
+        self.board = Board()
+        self.player = Player(3) 
+        self.apple = Apple(5,5)
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+ 
+        pygame.display.set_caption('No Step on Snek')
+        self._running = True
+        self._image_surf = pygame.image.load("block.png").convert()
+        self._apple_surf = pygame.image.load("apple.png").convert()
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+ 
+    def on_loop(self):
+        self.player.update()
+        
+        # does snake run into walls?
+        #for i in range[]
+ 
+        # does snake eat apple?
+        for i in range(0,self.player.length):
+            if self.board.isCollision(self.apple.x,self.apple.y,self.player.x[i], self.player.y[i],44):
+                self.apple.x = randint(2,9) * 44
+                self.apple.y = randint(2,9) * 44
+                self.player.length = self.player.length + 1
+ 
+ 
+        # does snake collide with itself?
+        for i in range(2,self.player.length):
+            if self.board.isCollision(self.player.x[0],self.player.y[0],self.player.x[i], self.player.y[i],40):
+                print("You lose! Collision: ")
+                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
+                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
+                # this exist the game and closes the window 
+                #self._running = Falseø
+                exit(0)
+        pass
+ 
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self.player.draw(self._display_surf, self._image_surf)
+        self.apple.draw(self._display_surf, self._apple_surf)
+        pygame.display.flip()
+ 
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed() 
+ 
+            if (keys[K_RIGHT]):
+                self.player.moveRight()
+ 
+            if (keys[K_LEFT]):
+                self.player.moveLeft()
+ 
+            if (keys[K_UP]):
+                self.player.moveUp()
+ 
+            if (keys[K_DOWN]):
+                self.player.moveDown()
+ 
+            if (keys[K_ESCAPE]):
+                self._running = False
+ 
+            self.on_loop()
+            self.on_render()
+ 
+            time.sleep (50.0 / 1000.0);
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    theApp = SnakeApp()
+    theApp.on_execute()
